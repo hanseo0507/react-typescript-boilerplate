bc16026607ba4d2e98afba302650cbb7b183f5da
HanSeo0507 <i.hansu.lee@gmail.com>
2022-01-12 15:21:05 +0900

    feat: add AppRouter component using react-router-dom(v6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "./styles/globalStyle";
+import { AppRouter } from "./routes";
 
 export const App: React.FC = () => {
 	return (
 		<>
 			<GlobalStyle />
+			<AppRouter />
 		</>
 	);
 };
---
+++ src/routes/router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export const AppRouter: React.FC = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<>Hello, React</>} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
