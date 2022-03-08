7fb70fe3c2adf372f059bfcc551c5f6bfd1f0460
HanSeo0507 <i.hansu.lee@gmail.com>
2022-03-08 17:32:43 +0900

    edit: change `styled-reset` to `styled-components` of global style

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from "styled-components";
-import { reset } from "styled-reset";
+import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
-    ${reset}
+    ${normalize}
 
     * {
         box-sizing: border-box;
